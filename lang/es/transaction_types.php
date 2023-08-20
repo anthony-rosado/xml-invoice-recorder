a54be468f22044d024e093054f85fc4595494108
Anthony Rosado <arosado-26@hotmail.com>
2023-08-20 13:47:26 -0500

    Add transaction types language file
    Add code names in spanish

---
+++ lang/es/transaction_types.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'code_names' => [
+        'internal_sales' => 'Venta Interna',
+        'exportation' => 'Exportación',
+        'not_domiciled' => 'No Domiciliados',
+        'internal_sales_advances' => 'Venta Interna – Anticipos',
+        'itinerant_sale' => 'Venta Itinerante',
+        'invoice_guide' => 'Factura Guía',
+        'rice_sale' => 'Venta Arroz Pilado',
+        'perception_receipt_invoice' => 'Factura Comprobante de Percepción',
+        'invoice_shipper_guide' => 'Factura - Guía remitente',
+    ],
+];
