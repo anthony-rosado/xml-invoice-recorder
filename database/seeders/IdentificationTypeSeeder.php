ec548f6fa5511504568b19cacf332c1c03878401
Anthony Rosado <arosado-26@hotmail.com>
2023-08-20 16:06:15 -0500

    Add identification type seeder
    Make use of enum class, language file and model to store records

---
+++ database/seeders/IdentificationTypeSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Contracts\IdentificationTypeCode;
+use App\Models\IdentificationType;
+use Illuminate\Database\Seeder;
+
+class IdentificationTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $identificationTypes = [
+            [
+                'code' => IdentificationTypeCode::NoDocument->value,
+                'name' => __('identification_types.code_names.no_document', [], 'es'),
+            ],
+            [
+                'code' => IdentificationTypeCode::Dni->value,
+                'name' => __('identification_types.code_names.dni', [], 'es'),
+            ],
+            [
+                'code' => IdentificationTypeCode::ForeignerCard->value,
+                'name' => __('identification_types.code_names.foreigner_card', [], 'es'),
+            ],
+            [
+                'code' => IdentificationTypeCode::Ruc->value,
+                'name' => __('identification_types.code_names.ruc', [], 'es'),
+            ],
+            [
+                'code' => IdentificationTypeCode::Passport->value,
+                'name' => __('identification_types.code_names.passport', [], 'es'),
+            ],
+        ];
+
+        foreach ($identificationTypes as $identificationType) {
+            IdentificationType::query()->create($identificationType);
+        }
+    }
+}
