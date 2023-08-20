b1709accf1a4593bbea0285a8636db5a98c22eda
Anthony Rosado <arosado-26@hotmail.com>
2023-08-20 16:35:43 -0500

    Add taxes language file
    Add code names in spanish

---
+++ lang/es/taxes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'code_names' => [
+        'igv' => 'IGV',
+        'isc' => 'ISC',
+        'free' => 'Gratuito',
+        'exonerated' => 'Exonerado',
+        'unaffected' => 'Inafecto',
+        'other' => 'Otros cargos',
+    ],
+];
