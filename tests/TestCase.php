284716ec22f09f78860cbf717f2d114249b30e4d
Anthony Rosado <arosado-26@hotmail.com>
2023-08-25 01:45:18 -0500

    Update test case
    Configure database refresh, database seeding and passport installation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    protected bool $seed = true;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('passport:install');
+    }
 }
