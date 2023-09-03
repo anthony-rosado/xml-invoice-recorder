f51b764822182d09c43f80759a165987d9dbec97
Anthony Rosado <arosado-26@hotmail.com>
2023-09-03 15:38:18 -0500

    Add factory to invoice model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -48,6 +49,8 @@ use Illuminate\Support\Carbon;
  */
 class Invoice extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'series',
         'correlative_number',
