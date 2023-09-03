5081e775a4e9b886dc0a05e4f298e1e9ba36c9e7
Anthony Rosado <arosado-26@hotmail.com>
2023-09-03 15:40:49 -0500

    Add feature test for invoice item service

---
+++ tests/Feature/Services/InvoiceItemServiceTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Tests\Feature\Services;
+
+use App\Enums\TaxCode;
+use App\Models\Acquirer;
+use App\Models\Currency;
+use App\Models\DocumentType;
+use App\Models\Invoice;
+use App\Models\InvoiceItem;
+use App\Models\Issuer;
+use App\Models\Tax;
+use App\Models\TransactionType;
+use App\Models\User;
+use App\Services\InvoiceItemService;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class InvoiceItemServiceTest extends TestCase
+{
+    use WithFaker;
+
+    public function testCreateInvoiceItem(): void
+    {
+        $transactionType = TransactionType::query()->inRandomOrder()->first();
+        $documentType = DocumentType::query()->inRandomOrder()->first();
+        $currency = Currency::query()->inRandomOrder()->first();
+        $issuer = Issuer::factory()->createOne();
+        $acquirer = Acquirer::factory()->createOne();
+        $user = User::factory()->createOne();
+        $invoice = Invoice::factory()
+            ->for($transactionType)
+            ->for($documentType)
+            ->for($currency)
+            ->for($issuer)
+            ->for($acquirer)
+            ->for($user)
+            ->createOne();
+        $invoiceItem = InvoiceItem::factory()->makeOne();
+
+        $service = app(InvoiceItemService::class);
+        $service->create($invoiceItem->attributesToArray(), $invoice);
+
+        $this->assertDatabaseHas(
+            'invoice_items',
+            [
+                'code' => $invoiceItem->code,
+                'invoice_id' => $invoice->getKey(),
+            ]
+        );
+    }
+
+    public function testAddTaxesToInvoiceItem(): void
+    {
+        $transactionType = TransactionType::query()->inRandomOrder()->first();
+        $documentType = DocumentType::query()->inRandomOrder()->first();
+        $currency = Currency::query()->inRandomOrder()->first();
+        $issuer = Issuer::factory()->createOne();
+        $acquirer = Acquirer::factory()->createOne();
+        $user = User::factory()->createOne();
+        $invoice = Invoice::factory()
+            ->for($transactionType)
+            ->for($documentType)
+            ->for($currency)
+            ->for($issuer)
+            ->for($acquirer)
+            ->for($user)
+            ->createOne();
+        $invoiceItem = InvoiceItem::factory()
+            ->for($invoice)
+            ->createOne();
+
+        $itemTaxes = [
+            ['code' => TaxCode::Igv->value, 'amount' => $this->faker->randomFloat()],
+            ['code' => TaxCode::Isc->value, 'amount' => $this->faker->randomFloat()],
+        ];
+
+        $taxes = Tax::query()->whereIn('code', Arr::pluck($itemTaxes, 'code'))->get();
+
+        $service = app(InvoiceItemService::class);
+        $service->setInvoiceItem($invoiceItem);
+        $service->addTaxes($itemTaxes);
+
+        foreach ($taxes as $tax) {
+            $this->assertDatabaseHas(
+                'invoice_item_tax',
+                [
+                    'item_id' => $invoiceItem->getKey(),
+                    'tax_id' => $tax->getKey(),
+                ]
+            );
+        }
+    }
+}
